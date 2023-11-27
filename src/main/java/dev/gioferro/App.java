b712c72ef8c8d25b6d471531e64d7776465d8bc8
Giovanni Ferro <giovanniferro5@gmail.com>
2023-11-26 20:59:43 -0400

    Código alterado

--- src/main/java/dev/gioferro/App.java
+++ src/main/java/dev/gioferro/App.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
         Filme starWars = new Filme("Star Wars", 1977);
-        Usuario gioferro = new Usuario("gioferro");
+        Usuario usuario1 = new Usuario("gioferro");
         Usuario teste = new Usuario("teste");
-        Review review1 = new Review(gioferro, 10, "Ótimo Filme");
-        Review review2 = new Review(teste, 7, "Gosto muito");
-        starWars.adicionaReview(review1);
-        starWars.adicionaReview(review2);
+        Review review1 = new Review(usuario1, 10, "Ótimo Filme");
+        Review review27 = new Review(teste, 7, "Gosto muito");
         
-        System.out.println("** APP DE REVIEW DE FILMES **");
+        starWars.adicionaReview(review1);
+        starWars.adicionaReview(review27);
     }
 }
